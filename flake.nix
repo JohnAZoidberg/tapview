4157ad0edc1ef840afb85732f04ac7db566cbbc9
Daniel Schaefer <dhs@frame.work>
2026-02-11 21:39:41 +0800

    nix-flake: Also build and run, not just dev shell
    See readme for details
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "DevShell for shanti";
+  description = "Tapview - touchpad heatmap visualizer";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -20,26 +20,51 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        runtimeLibs = with pkgs; [
+          libGL
+          libx11
+          libxcursor
+          libxrandr
+          libxi
+          libxcb
+          libxkbcommon
+          vulkan-loader
+          wayland
+        ];
       in
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "tapview";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            makeWrapper
+          ];
+
+          buildInputs = with pkgs; [
+            systemd
+            libinput
+          ] ++ runtimeLibs;
+
+          postInstall = ''
+            wrapProgram $out/bin/tapview \
+              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath runtimeLibs}
+          '';
+        };
+
         devShells.default =
           with pkgs;
           mkShell rec {
             buildInputs = [
               pkg-config
               rust-bin.nightly.latest.default
-	      systemd
-	      libinput
-
-              libx11
-              libxcursor
-              libxrandr
-              libxi
-              libxcb
-              libxkbcommon
-              vulkan-loader
-              wayland
-            ];
+              systemd
+              libinput
+            ] ++ runtimeLibs;
 
             shellHook = ''
               export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
