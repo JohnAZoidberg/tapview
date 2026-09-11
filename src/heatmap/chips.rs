c62501d40d65585b7aec5cb52017136e9b857d68
Daniel Schaefer <dhs@frame.work>
2026-09-11 08:21:54 +0800

    fix clippy
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/heatmap/chips.rs
+++ src/heatmap/chips.rs
@@ -93,7 +93,7 @@ pub async fn read_frame<D: HidDevice>(
 
     // Convert LE bytes to i16
     Ok(raw
-        .chunks_exact(2)
+        .as_chunks::<2>().0.iter()
         .map(|pair| i16::from_le_bytes([pair[0], pair[1]]))
         .collect())
 }
